258d33f9d39fbbae147239770edb4f37e86129b1
AlexSirghie <alex.sirghie22@gmail.com>
2022-03-04 15:48:51 +0200

    Comit1

---
+++ angular-electron/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { AppRoutingModule } from './app-routing.module';
+
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+
+import { FormsModule } from '@angular/forms'; 
+import { RoundProgressModule } from 'angular-svg-round-progressbar'; 
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule, 
+    FormsModule, 
+    RoundProgressModule,
+    AppRoutingModule 
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
